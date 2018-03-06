2b3da4371b8a27613fc53cccf8ace36ee1a504ea
philyoosays <philyoomail@gmail.com>
2018-03-06 18:34:36 -0500

    memory game final copy

--- js/main.js
+++ js/main.js
@@ -17,24 +17,71 @@ var cards = [{
 }];
 
 var cardsInPlay = [];
+var playerScore = 0;
+var scoreText = document.getElementById('scoreBoard');
 
 var checkForMatch = function(){
 	if(cardsInPlay.length === 2){
 		if(cardsInPlay[0] === cardsInPlay[1]){
-			alert("You found a match!");
+			playerScore+=1;
+			showPlayerScore();
+			turnOrange();
+			document.getElementById('winOrLose').textContent = "MATCH!";
+			// alert("You found a match!");
 		}else {
-			alert("Sorry, try again.")
+			document.getElementById('winOrLose').textContent = "TRY AGAIN!";
 		}
 	}
 }
 
-var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+var turnOrange = function(){
+	scoreText.style.color = "#F15B31";
+}
+
+var turnTeal = function(){
+	scoreText.style.color = "#00A6B3";
+}
+
+var showPlayerScore = function(){
+	scoreText.textContent = "Score: " + playerScore;
+}
+
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id')
+	this.setAttribute('src',cards[cardId].cardImage)
 	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for(var i = 0; i<cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+var playAgain = function(){
+	cardsInPlay = [];
+	turnTeal();
+	document.getElementById('winOrLose').textContent = "Good Luck!";
+	var flippedCard = document.getElementsByTagName('img');
+	for(var i = 0; i<flippedCard.length; i++){
+		flippedCard[i].setAttribute('src','images/back.png');
+	}
+}
+
+var resetScore = function(){
+	playerScore = 0;
+	showPlayerScore();
+	playAgain();
+}
+
+showPlayerScore();
+createBoard();
+document.querySelector("#resetButton").addEventListener('click',playAgain);
+document.querySelector("#restart").addEventListener('click',resetScore);
+
+
